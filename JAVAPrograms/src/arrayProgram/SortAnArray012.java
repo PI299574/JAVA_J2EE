f6a4e7d455821d72ed77957960d01728abfbfcff
pintu <pintu@192.168.1.113>
2020-01-09 22:27:06 +0530

    dutch national flag problem

--- JAVAPrograms/src/arrayProgram/SortAnArray012.java
+++ JAVAPrograms/src/arrayProgram/SortAnArray012.java
@@ -12,11 +12,46 @@ public class SortAnArray012 {
 		// TODO Auto-generated method stub
       int []arr={0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
       
-      arr=dutchFlagSort(arr);
+      //arr=dutchFlagSort(arr);
+      arr=dutchFlagSortMySolution(arr);
       for(int i=0;i<arr.length;i++){
     	  System.out.print(arr[i]+" ");
       }
       
+	}
+
+	private static int[] dutchFlagSortMySolution(int[] arr) {
+		// TODO Auto-generated method stub
+		
+		
+	/*	steps: https://coderbyte.com/algorithm/dutch-national-flag-sorting-problem
+			1. create 2 pointer low and high point to first and last elemnt.
+			2. create a mid pointer , point to first elemnt.
+			3. if arr[mid] == 2...swap(arr[mid],arr[high])...decrement high--.
+			4. if arr[mid] == 0 ...swap(arr[mid],arr[high])...increment mid++, low++;
+			5. if arr[mid] == 1 ...do nothing...just increment mid++;
+	*/
+		
+		int low=0;int high=arr.length-1;
+		int mid=0;
+		while(mid<=high){
+		if(arr[mid]==2){
+			int temp= arr[mid];
+			arr[mid] =arr[high];
+			arr[high] = temp;
+			high--;
+		}
+		else if(arr[mid]==0){
+			int temp= arr[mid];
+			arr[mid] =arr[low];
+			arr[low] = temp;
+			low++;mid++;
+		}
+		else if(arr[mid]==1){
+			mid++;
+		}
+		}
+		return arr;
 	}
 
 	private static int[] dutchFlagSort(int[] arr) {
