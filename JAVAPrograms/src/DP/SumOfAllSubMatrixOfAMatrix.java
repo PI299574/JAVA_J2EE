830901f4ed22a29f30218dd84340e2b1814279e3
pintu <pintu@192.168.1.107>
2020-02-19 23:31:16 +0530

    submatrix sum

---
+++ JAVAPrograms/src/DP/SumOfAllSubMatrixOfAMatrix.java
@@ -0,0 +1,70 @@
+package DP;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+/*
+ https://www.youtube.com/watch?v=PwDqpOMwg6U&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr&index=40
+ https://www.techiedelight.com/calculate-sum-elements-sub-matrix-constant-time/
+ */
+public class SumOfAllSubMatrixOfAMatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] mat =
+			{
+				{ 0, 2, 5, 4, 1 },
+				{ 4, 8, 2, 3, 7 },
+				{ 6, 3, 4, 6, 2 },
+				{ 7, 3, 1, 8, 3 },
+				{ 1, 5, 7, 9, 4 }
+			};
+
+			// (r1, c1) and (r2, c2) represents top-left and bottom-right
+			// coordinates of the sub-matrix
+			int r1 = 1, c1 = 1, r2 = 3, c2 = 3;
+
+			// calculate sub-matrix sum
+			
+			findSubmatrixSumfrom00(mat);
+			//System.out.print(findSubmatrixSum(mat, r1, c1, r2, c2));
+			int sum=findsumofAllSubmatrixbetweenSourceAndDestination(mat,r1, c1, r2, c2);
+			System.out.println("Sum is "+sum);
+		}
+	
+	static int sum[][];
+	private static void findSubmatrixSumfrom00(int[][] mat) {
+		// TODO Auto-generated method stub
+		int row=0;int col=0;
+		if(mat.length!=0){
+			row=mat.length;
+			col=mat[0].length;
+		}
+	    sum=new int[row+1][col+1];
+		for(int i=1;i<sum.length;i++){
+			for(int j=1;j<sum[i].length;j++){
+				sum[i][j]=mat[i-1][j-1]+sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1];
+			}
+		}
+	    
+	    System.out.println("Sum of all submtrix from {0,0} is **************");
+		for(int i=0;i<sum.length;i++){
+			for(int j=0;j<sum[i].length;j++){
+				System.out.print(sum[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	private static int findsumofAllSubmatrixbetweenSourceAndDestination(
+			int[][] mat, int r1, int c1, int r2, int c2) {
+		// TODO Auto-generated method stub
+	
+		r1++;
+		r2++;
+		c1++;
+		c2++;
+		return (sum[r2][c2]-sum[r2][c1-1]-sum[r1-1][c2]+sum[r1-1][c1-1]);
+		
+	}
+}
+
+
